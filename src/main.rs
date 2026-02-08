f312823d1d9085f09833bf703ca24d91ea3543a2
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2026-02-08 22:22:59 +0100

    working rtic hello world on uart gpio 0

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,24 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
+use fugit::RateExtU32;
 use panic_probe as _;
-use rp235x_hal::clocks::init_clocks_and_plls;
-use rp235x_hal::{self as hal, entry};
-use rp235x_hal::{Clock, pac};
-use rp235x_hal::{uart::{DataBits, StopBits, UartConfig}};
-use fugit::{RateExtU32};
+use rp235x_hal::{
+    self as hal, Clock,
+    clocks::init_clocks_and_plls,
+    sio::Sio,
+    uart::{DataBits, StopBits, UartConfig},
+    watchdog::Watchdog,
+};
+use rtic_monotonics::rp235x::prelude::*;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-// use some_bsp;
+rp235x_timer_monotonic!(Mono);
 
 /// Tell the Boot ROM about our application
 #[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
-
-#[entry]
-fn main() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = cortex_m::Peripherals::take().unwrap();
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-    let sio = hal::Sio::new(pac.SIO);
-
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    let uart_pins = (
-        pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
-        pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
-    );
-
-    let baudrate:u32 = 115_200;
-
-    let uart = hal::uart::UartPeripheral::new(
-        pac.UART0,
-        uart_pins,
-        &mut pac.RESETS,
-    )
-        .enable(
-            UartConfig::new(baudrate.Hz(), DataBits::Eight, None, StopBits::One),
-            clocks.peripheral_clock.freq(),
-        )
-        .unwrap();
-
-    loop {
-        uart.write_full_blocking(b"Hello rp235x UART!\r\n");
-        delay.delay_ms(1000);
-    }
-}
 
 /// Program metadata for `picotool info`
 #[unsafe(link_section = ".bi_entries")]
@@ -80,9 +26,93 @@ fn main() -> ! {
 pub static PICOTOOL_ENTRIES: [rp235x_hal::binary_info::EntryAddr; 5] = [
     rp235x_hal::binary_info::rp_cargo_bin_name!(),
     rp235x_hal::binary_info::rp_cargo_version!(),
-    rp235x_hal::binary_info::rp_program_description!(c"RP2350 hello world"),
+    rp235x_hal::binary_info::rp_program_description!(c"RP2350 RTIC hello world"),
     rp235x_hal::binary_info::rp_cargo_homepage_url!(),
     rp235x_hal::binary_info::rp_program_build_attribute!(),
 ];
+
+#[rtic::app(device = rp235x_hal::pac, peripherals = true, dispatchers = [TIMER0_IRQ_1])]
+mod app {
+    use super::*;
+
+    type Uart0 = hal::uart::UartPeripheral<
+        hal::uart::Enabled,
+        hal::pac::UART0,
+        (
+            hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::FunctionUart, hal::gpio::PullDown>,
+            hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::FunctionUart, hal::gpio::PullDown>,
+        ),
+    >;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
+        uart: Uart0,
+    }
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
+        Mono::start(ctx.device.TIMER0, &mut ctx.device.RESETS);
+        // Configure the clocks, watchdog - The default is to generate a 125 MHz system clock
+        let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
+
+        // External high-speed crystal on the pico board is 12Mhz
+        let external_xtal_freq_hz = 12_000_000u32;
+        let clocks = init_clocks_and_plls(
+            external_xtal_freq_hz,
+            ctx.device.XOSC,
+            ctx.device.CLOCKS,
+            ctx.device.PLL_SYS,
+            ctx.device.PLL_USB,
+            &mut ctx.device.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let sio = Sio::new(ctx.device.SIO);
+
+        let pins = hal::gpio::Pins::new(
+            ctx.device.IO_BANK0,
+            ctx.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut ctx.device.RESETS,
+        );
+
+        let uart_pins = (
+            pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
+            pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
+        );
+
+        let baudrate: u32 = 115_200;
+
+        let uart = hal::uart::UartPeripheral::new(ctx.device.UART0, uart_pins, &mut ctx.device.RESETS)
+            .enable(
+                UartConfig::new(baudrate.Hz(), DataBits::Eight, None, StopBits::One),
+                clocks.peripheral_clock.freq(),
+            )
+            .unwrap();
+
+        (Shared {}, Local { uart })
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            hello::spawn().ok();
+        }
+    }
+
+    #[task(
+        local = [uart],
+        priority = 1
+    )]
+    async fn hello(ctx: hello::Context) {
+        ctx.local.uart.write_full_blocking(b"Hello RTIC UART!\r\n");
+        Mono::delay(1000.millis()).await;
+    }
+}
 
 // End of file
